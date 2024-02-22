772d305d1d9c993e7b83266b6d39447fde01b07b
Sarut Somprasert <65011019@KMITL.AC.TH>
2024-02-22 17:35:55 +0700

    s : testing MVC

---
+++ MVC_Test_Zone/RaidFinder/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RaidFinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Description()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult PlayerStat()
+        {
+            return View();
+        }
+
+    }
+}
