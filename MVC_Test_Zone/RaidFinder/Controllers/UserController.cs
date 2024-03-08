02e20df0a362c2c7fc285e676babb197b00701de
SarutSomprasert <160750797+SarutSomprasert@users.noreply.github.com>
2024-03-09 00:37:23 +0700

    s : UNTESTED - add image upload and winning?

--- MVC_Test_Zone/RaidFinder/Controllers/UserController.cs
+++ MVC_Test_Zone/RaidFinder/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using RaidFinder.Models;
+using System.Data.SqlClient;
 using System.Reflection;
 
 namespace RaidFinder.Controllers
@@ -13,12 +14,67 @@ namespace RaidFinder.Controllers
 
             return View(Users);
         }
-        public IActionResult Profile(int? id)
+        public IActionResult UploadImage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(Image model, IFormFile file)
+		{
+			if (file == null || file.Length == 0)
+			{
+				return RedirectToAction(nameof(Index));
+			}
+
+			using (var memoryStream = new MemoryStream())
+			{
+				await file.CopyToAsync(memoryStream);
+				model.ImageData = memoryStream.ToArray();
+			}
+
+			using (var connection = new SqlConnection("Server=localhost;Database=UserDB;Trusted_Connection=True;"))
+			{
+				await connection.OpenAsync();
+
+				using (var command = new SqlCommand("INSERT INTO Images (UserId, ImageData) VALUES (@OwnerId, @ImageData)", connection))
+				{
+					command.Parameters.AddWithValue("@OwnerId", model.OwnerId);
+					command.Parameters.AddWithValue("@ImageData", model.ImageData);
+
+					await command.ExecuteNonQueryAsync();
+				}
+			}
+
+
+			return RedirectToAction(nameof(Index));
+		}
+
+        private byte[] GetImageById(int? id)
+        {
+            byte[] image = null;
+            using(var connection = new SqlConnection("Server=localhost;Database=UserDB;Trusted_Connection=True;"))
+            {
+                connection.Open();
+                using (var cmd = new SqlCommand("SELECT ImageData FROM Images WHERE UserId = @UserId", connection))
+                {
+                    cmd.Parameters.AddWithValue("@UserId", id);
+                    image = (byte[])cmd.ExecuteScalar();
+                }
+            }
+            return image;
+        }
+		public IActionResult Profile(int? id)
         {
             UserDB.UpdateDB();
             var User = UserDB.GetUserCopyById((int)id);
+            var Image = GetImageById(id);
+            var viewModel = new ProfileViewModel
+            {
+                User = User,
+                Image = Image
+            };
 
-            return View(User);
+            return View(viewModel);
         }
         public IActionResult EditProfile(int? id)
         {
