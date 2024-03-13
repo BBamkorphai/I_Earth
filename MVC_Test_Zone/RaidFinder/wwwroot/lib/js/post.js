7b319aa2f2202310230af66cc7a3be7dfb6a8712
pithakpong <111265617+pithakpong@users.noreply.github.com>
2024-03-14 01:31:59 +0700

    edit boss image in view index html and complete edit post add post

--- MVC_Test_Zone/RaidFinder/wwwroot/lib/js/post.js
+++ MVC_Test_Zone/RaidFinder/wwwroot/lib/js/post.js
@@ -3,7 +3,6 @@ function test() {
     var selectedValue = document.getElementById("boss").value;
     var postname = document.getElementById("Name").value;
     var concatinated = selectedValue + "," + postname;
-    document.getElementById("concatenatedValue").innerText = concatinated;
     document.getElementById("postName").value = concatinated;
     console.log("concatinated : " + concatinated);
 
